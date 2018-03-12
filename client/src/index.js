f373c272077f4b112127b8db4dfb1e4eca7945c8
SaaSha <saasha.pina@gmail.com>
2018-03-12 15:58:01 -0400

    SaaSha Pina W.I.P

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
 
 ReactDOM.render(
   <div>Application running, add your routing</div>,
